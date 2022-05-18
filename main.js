43d61e0dcab3ee61dfc06ae4c0b5bd94e964afce
Disha1998 <dishasathavara1998@gmail.com>
2022-05-18 12:15:16 +0530

    display the balance which I got in console

--- main.js
+++ main.js
@@ -13,6 +13,17 @@ async function login() {
 
         const balances = await Moralis.Web3.getAllERC20({ address: "0xC1118c54B2FE35941C4032A7b9622eC55277fd97" });
         console.log('balances===>', balances);
+        if (balances !== undefined) {
+            const result = document.getElementById("result");
+
+            balances.map((r) => {
+                const newDiv = document.createElement("div");
+
+                newDiv.innerHTML = "Balance :" + r.balance + ",  <br>  " + "Decimals : " + r.decimals + ", <br>" + "Name : " + r.name + ",  <br>" + "Symbol : " + r.symbol + "<br><br>";
+                result.appendChild(newDiv);
+            })
+
+        }
 
     } catch (error) {
         console.log(error);
